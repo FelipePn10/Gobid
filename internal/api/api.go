a3ff1c52d5430336ea141096e5641d65b2400b60
Panosso <felipepanosso21@gmail.com>
2024-12-11 22:04:33 -0300

    v3

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"github.com/FelipePn10/Gobid/internal/services"
 	"github.com/go-chi/chi/v5"
 )
 
 type Api struct {
-	Router *chi.Mux
+	Router      *chi.Mux
+	UserService services.UserService
 }
--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,9 +1,36 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"github.com/FelipePn10/Gobid/internal/jsonutils"
+	"github.com/FelipePn10/Gobid/internal/services"
+	"github.com/FelipePn10/Gobid/internal/usecase/user"
+	"net/http"
+)
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented")
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+	id, err := api.UserService.CreateUser(r.Context(),
+		data.UserName,
+		data.Email,
+		data.Password,
+		data.Bio,
+	)
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicateEmailOrPassword) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "invalid email or password",
+			})
+			return
+		}
+	}
+	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_id": id,
+	})
 }
 
 func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
